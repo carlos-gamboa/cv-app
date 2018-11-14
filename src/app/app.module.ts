5f140e3c7549e9b6efa321d2b66bad1621320605
mecanos28 <31737854+mecanos28@users.noreply.github.com>
2018-11-13 21:58:57 -0600

    Firebase configuration information added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,11 +25,12 @@ import { AngularFireModule } from 'angularfire2';
 
 // Firebase config
 export const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: "AIzaSyB2rqJRJVLJsjQT8SQYQ6pJXTub41A81p4",
+  authDomain: "cv-app-40b38.firebaseapp.com",
+  databaseURL: "https://cv-app-40b38.firebaseio.com",
+  projectId: "cv-app-40b38",
+  storageBucket: "cv-app-40b38.appspot.com",
+  messagingSenderId: "557048975608"
 };
 
 @NgModule({
