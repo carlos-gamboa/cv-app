8b94fb50eac81a4995a0e525909fbfb6472129d0
carlos-gamboa <cali1997gv@hotmail.com>
2018-11-17 16:27:55 -0600

    Display Register Feature

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  @ViewChild('f') signinForm: NgForm;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.signinForm.value);
   }
 
 }
