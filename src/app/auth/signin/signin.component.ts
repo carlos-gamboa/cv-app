981b5a2b444dee5f8c69314f92c3e7e11bbd5617
carlos-gamboa <cali1997gv@hotmail.com>
2018-11-17 15:26:28 -0600

    Display Login Feature

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninComponent implements OnInit {
 
+  @ViewChild('f') signinForm: NgForm;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.signinForm.value);
   }
 
 }
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  displayMenu =  false;
+
   constructor() { }
 
   ngOnInit() {
