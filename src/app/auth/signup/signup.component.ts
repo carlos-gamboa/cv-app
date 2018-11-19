3179eac99e717385699979b4b34903ff718e7dca
carlos-gamboa <cali1997gv@hotmail.com>
2018-11-19 17:07:49 -0600

    Show alerts on SignUp

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,8 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {AuthService} from '../../services/auth.service';
-import {Router} from "@angular/router";
-import {Cv} from '../../models/cv.model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -21,12 +20,15 @@ export class SignupComponent implements OnInit {
   }
 
   onSubmit() {
-    this.authService.signupUser(this.signinForm.controls['username'].value, this.signinForm.controls['password'].value)
+    this.authService.signupUser(this.signinForm.controls['username2'].value, this.signinForm.controls['password2'].value)
       .then((user: any) => {
       this.user = user;
-        console.log(this.user);
-      if (this.user != null){
-        this.router.navigate(['/cv']);
+      if (this.user != null) {
+        setTimeout(
+          () => {
+            this.router.navigate(['/home']);
+        }
+        , 1500);
       } else {
         this.registerError = true;
         console.log('Register Failed');
