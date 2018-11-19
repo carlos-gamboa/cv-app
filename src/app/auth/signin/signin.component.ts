ef984d9128af003a47341301fcdbd551b6bd3d80
mecanos28 <31737854+mecanos28@users.noreply.github.com>
2018-11-18 18:05:24 -0600

    Finished A-3. Lógica de autenticación

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-signin',
@@ -9,14 +11,40 @@ import {NgForm} from '@angular/forms';
 export class SigninComponent implements OnInit {
 
   @ViewChild('f') signinForm: NgForm;
+  token: any = null;
+  loginError: boolean = false;
 
-  constructor() { }
+  @Output() childForm = new EventEmitter();
+
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    console.log(this.signinForm.value);
+    this.authService.signinUser(this.signinForm.controls['username'].value, this.signinForm.controls['password'].value)
+      .then((token: any)=>{
+        this.loginError = false;
+        this.token = this.authService.getToken();
+        console.log(this.authService.getToken());
+        console.log(this.token);
+        if (this.token != null){
+          this.isLoggedIn();
+          this.router.navigate(['/cv']);
+        }
+        else{
+          this.loginError = true;
+          console.log("Login Failed");
+        }
+      }).catch( reason => {
+      this.loginError = true;
+      console.log("Login Failed");
+    });
+  }
+
+  isLoggedIn() {
+    this.childForm.emit(true);
   }
 
 }
