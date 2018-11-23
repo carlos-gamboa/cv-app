d2d1c78958c40c6841d358f3875f8b58762c4244
carlos-gamboa <cali1997gv@hotmail.com>
2018-11-22 18:00:51 -0600

    Display information on Design 2

--- src/app/cv/template-vertical/template-vertical.component.ts
+++ src/app/cv/template-vertical/template-vertical.component.ts
@@ -13,35 +13,35 @@ import {ActivatedRoute, Router} from '@angular/router';
   ]
 })
 export class TemplateVerticalComponent implements OnInit {
-  home: ElementRef;
-  about: ElementRef;
-  resume: ElementRef;
-  publications: ElementRef;
-  interests: ElementRef;
-  contact: ElementRef;
-
-  @Input() cv: Cv;
-  @ViewChild('home') set contentHome(content: ElementRef) {
-    this.home = content;
-  }
-  @ViewChild('about') set contentAbout(content: ElementRef) {
-    this.about = content;
-  }
-  @ViewChild('resume') set contentResume(content: ElementRef) {
-    this.resume = content;
-  }
-  @ViewChild('publications') set contentPublications(content: ElementRef) {
-    this.publications = content;
-  }
-  @ViewChild('interests') set contentInterests(content: ElementRef) {
-    this.interests = content;
-  }
-  @ViewChild('contact') set contentContact(content: ElementRef) {
-    this.contact = content;
-  }
   elements: any[] = [];
   currentSection: string;
   id: string;
+
+  @Input() cv: Cv;
+  @ViewChild('home') set contentHome(content: ElementRef) {
+    this.elements['home'] = content;
+  }
+  @ViewChild('about') set contentAbout(content: ElementRef) {
+    this.elements['about'] = content;
+  }
+  @ViewChild('resume') set contentResume(content: ElementRef) {
+    this.elements['resume'] = content;
+  }
+  @ViewChild('skills') set contentSkills(content: ElementRef) {
+    this.elements['skills'] = content;
+  }
+  @ViewChild('certifications') set contentCertifications(content: ElementRef) {
+    this.elements['certifications'] = content;
+  }
+  @ViewChild('publications') set contentPublications(content: ElementRef) {
+    this.elements['publications'] = content;
+  }
+  @ViewChild('interests') set contentInterests(content: ElementRef) {
+    this.elements['interests'] = content;
+  }
+  @ViewChild('contact') set contentContact(content: ElementRef) {
+    this.elements['contact'] = content;
+  }
 
   constructor(
     private route: ActivatedRoute,
@@ -50,12 +50,6 @@ export class TemplateVerticalComponent implements OnInit {
 
   ngOnInit() {
     this.currentSection = 'home';
-    this.elements['home'] = this.home;
-    this.elements['about'] = this.about;
-    this.elements['resume'] = this.resume;
-    this.elements['publications'] = this.publications;
-    this.elements['interests'] = this.interests;
-    this.elements['contact'] = this.contact;
     this.route.fragment.subscribe(
       (fragment: string) => {
         if (fragment) {
@@ -71,25 +65,51 @@ export class TemplateVerticalComponent implements OnInit {
 
   @HostListener('window:scroll', ['$event'])
   doSomething(event) {
-    if (window.scrollY > (this.contact.nativeElement.getBoundingClientRect().top + window.scrollY)) {
+    if (
+      (this.elements['contact']) &&
+      (window.scrollY > (this.elements['contact'].nativeElement.getBoundingClientRect().top + window.scrollY))
+    ) {
       this.currentSection = 'contact';
-    } else if (window.scrollY > (this.interests.nativeElement.getBoundingClientRect().top + window.scrollY)) {
+    } else if (
+      (this.elements['interests']) &&
+      (window.scrollY > (this.elements['interests'].nativeElement.getBoundingClientRect().top + window.scrollY))
+    ) {
       this.currentSection = 'interests';
-    } else if (window.scrollY > (this.publications.nativeElement.getBoundingClientRect().top + window.scrollY)) {
+    } else if (
+      (this.elements['publications']) &&
+      (window.scrollY > (this.elements['publications'].nativeElement.getBoundingClientRect().top + window.scrollY))
+    ) {
       this.currentSection = 'publications';
-    } else if (window.scrollY > (this.resume.nativeElement.getBoundingClientRect().top + window.scrollY)) {
+    } else if (
+      (this.elements['certifications']) &&
+      (window.scrollY > (this.elements['certifications'].nativeElement.getBoundingClientRect().top + window.scrollY))
+    ) {
+      this.currentSection = 'certifications';
+    } else if (
+      (this.elements['resume']) &&
+      (window.scrollY > (this.elements['resume'].nativeElement.getBoundingClientRect().top + window.scrollY))
+    ) {
       this.currentSection = 'resume';
-    } else if (window.scrollY > (this.about.nativeElement.getBoundingClientRect().top + window.scrollY)) {
+    } else if (
+      (this.elements['skills']) &&
+      (window.scrollY > (this.elements['skills'].nativeElement.getBoundingClientRect().top + window.scrollY))
+    ) {
+      this.currentSection = 'skills';
+    } else if (
+      (this.elements['about']) &&
+      (window.scrollY > (this.elements['about'].nativeElement.getBoundingClientRect().top + window.scrollY))
+    ) {
       this.currentSection = 'about';
-    } else {
+    } else if (this.elements['home']) {
       this.currentSection = 'home';
     }
   }
 
   private scrollTo(element: string) {
-    this.elements[element].nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
-    console.log(this.elements[element].nativeElement.getBoundingClientRect().top + window.scrollY);
-    this.currentSection = element;
+    setTimeout(() => {
+      this.elements[element].nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'start' });
+      this.currentSection = element;
+    }, 50);
   }
 
 }
